9884955077e1f465ffb9c67c70ea4b7e5330741e
jruz9 <josec300@live.com>
2022-02-22 23:59:00 -0500

    created structure and requirement txt

---
+++ excersise-3/app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template,request
+
+
+
+app=Flask(__name__)
+
+
+@app.get("/")
+def index():
+    return render_template('index.html')
+
+
+
+@app.post('/registered')
+def register():
+    return None
+
+
